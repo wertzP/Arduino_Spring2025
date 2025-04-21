ba4ae2dd9afb687e972f279a3a1e095ecbccd50e
J. Nugen <joseph.nuyenp@gmail.com>
2025-04-21 14:00:04 -0400

    Blue LED

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -24,14 +24,14 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize digital pin 12 as an output.
+  pinMode(12, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(13000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
   delay(100);                      // wait for a second
 }
