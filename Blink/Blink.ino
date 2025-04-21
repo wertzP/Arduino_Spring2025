c72d0c1588c635eea8e795f88e1fa935eda60276
J. Nugen <joseph.nuyenp@gmail.com>
2025-04-21 14:04:58 -0400

    Updated Code with blueLED = 12
    Also made on/off time = to Mr. Lange's

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -21,17 +21,18 @@
 
   https://docs.arduino.cc/built-in-examples/basics/Blink/
 */
+int blueLED = 12;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 12 as an output.
-  pinMode(12, OUTPUT);
+  pinMode(blueLED, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(13000);                      // wait for a second
-  digitalWrite(12, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for a second
+  digitalWrite(blueLED, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(3000);                      // wait for a second
+  digitalWrite(blueLED, LOW);   // turn the LED off by making the voltage LOW
+  delay(500);                      // wait for a second
 }
